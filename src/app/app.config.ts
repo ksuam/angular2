562be2bee26c0b11cc7a579fa58ba6180bc72ba0
ksuam <ksuamartinez@gmail.com>
2024-09-04 14:08:31 -0500

    xde

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async" //esta linea se agrego para que funcionase el dropdown
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync()]
 };
